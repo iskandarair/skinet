f35542c084f956a1e891d54b6d9effb740918d0c
Iskandar Anvarov <anvarov-iskandar@mail.ru>
2021-02-05 05:56:48 +0500

    end of section 8

--- Skinet/Client/src/app/app.component.ts
+++ Skinet/Client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Skinet';
+export class AppComponent implements OnInit {
+
+  title: string = 'Skinet';
+  products: IProduct[] = [];
+  pagination?: IPagination;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<IPagination>("https://localhost:5001/api/products?PageSize=50").subscribe((response : IPagination)  => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
+    
 }
