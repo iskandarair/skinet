b87224341cd2e04a6f3e24326760c74032494347
Iskandar Anvarov <anvarov-iskandar@mail.ru>
2021-02-10 12:31:55 +0500

    end of section 10

--- Skinet/Client/src/app/shop/shop.service.ts
+++ Skinet/Client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import { map, delay } from "rxjs/operators";
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -40,6 +41,9 @@ export class ShopService {
       })
     )
     ;
+  }
+  getProductById(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + "products/" + id);
   }
   getBrands() {
     return this.http.get<IBrand[]>(this.baseUrl + "products/brands");
